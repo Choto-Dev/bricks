00ebe5743acadd96202bc719df1c098741fe2255
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-11-14 23:02:48 +0600

    chore: create carousel component.

---
+++ apps/web/src/registry/bricks/components/carousel.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+// import { Slot } from "@radix-ui/react-slot";
+import gsap from "gsap";
+import React from "react";
+
+type CarouselContextProps = {
+  tl: GSAPTimeline;
+  slideCount: number;
+  setSlideCount: React.Dispatch<React.SetStateAction<number>>;
+  currentSliderIndex: number;
+  setCurrentSliderIndex: React.Dispatch<React.SetStateAction<number>>;
+};
+const CarouselContext = React.createContext<CarouselContextProps | undefined>(
+  undefined
+);
+
+type CarouselRootProps = React.ComponentPropsWithRef<"div">;
+function CarouselRoot(props: CarouselRootProps) {
+  const [slideCount, setSlideCount] = React.useState(0);
+  const [currentSliderIndex, setCurrentSliderIndex] = React.useState(0);
+  const tl = gsap.timeline();
+
+  const value = {
+    tl,
+    slideCount,
+    setSlideCount,
+    currentSliderIndex,
+    setCurrentSliderIndex,
+  };
+
+  return (
+    <CarouselContext value={value}>
+      <div {...props} className="" />
+    </CarouselContext>
+  );
+}
+
+type CarouselSlidesProps = React.ComponentPropsWithRef<"div">;
+function CarouselSlides(props: CarouselSlidesProps) {
+  const ref = React.useRef<HTMLDivElement>(null);
+  const ctx = React.useContext(CarouselContext);
+
+  if (!ctx) {
+    throw new Error("Carousel.Slides should be in Carousel.Root");
+  }
+
+  React.useEffect(() => {
+    if (ref.current) {
+      ctx.setSlideCount(ref.current.children.length);
+    }
+  }, [ctx]);
+
+  useGSAP(() => {
+    if (ref.current) {
+      ctx.tl.fromTo(
+        `#${ref.current.children.item(ctx.currentSliderIndex)?.id}`,
+        {
+          opacity: 0,
+          y: 100,
+        },
+        {
+          opacity: 1,
+          y: 0,
+        }
+      );
+    }
+  });
+
+  return (
+    <div ref={ref} {...props} className="relative h-96 w-full overflow-clip" />
+  );
+}
+
+type CarouselItemProps = React.ComponentPropsWithRef<"div">;
+function CarouselItem(props: CarouselItemProps) {
+  const ctx = React.useContext(CarouselContext);
+
+  if (!ctx) {
+    throw new Error("Carousel.Item should be in Carousel.Root");
+  }
+
+  return (
+    <div
+      {...props}
+      className="absolute inset-0 flex h-96 w-full items-center justify-center bg-red-400"
+    />
+  );
+}
+
+function CarouselPreviousButton() {
+  const ctx = React.useContext(CarouselContext);
+
+  if (!ctx) {
+    throw new Error("Carousel.PreviousButton should be in Carousel.Root");
+  }
+
+  return <div>Back</div>;
+}
+
+function CarouselNextButton() {
+  const ctx = React.useContext(CarouselContext);
+
+  if (!ctx) {
+    throw new Error("Carousel.NextButton should be in Carousel.Root");
+  }
+
+  console.log(ctx.currentSliderIndex);
+
+  return (
+    <button
+      onClick={() => ctx.setCurrentSliderIndex(ctx.currentSliderIndex + 1)}
+      type="button"
+    >
+      Next
+    </button>
+  );
+}
+
+const Root = CarouselRoot;
+const Slides = CarouselSlides;
+const Item = CarouselItem;
+const PreviousButton = CarouselPreviousButton;
+const NextButton = CarouselNextButton;
+
+export { Root, Slides, Item, PreviousButton, NextButton };
