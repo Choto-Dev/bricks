4944ff528b96858d9168de1078d2b32e5eef6567
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-11-16 23:22:38 +0600

    feat: add header component.

---
+++ apps/web/src/registry/bricks/components/header.tsx
@@ -0,0 +1,38 @@
+import { Slot } from "@radix-ui/react-slot";
+import type React from "react";
+import { cn } from "@/lib/utils";
+
+type HeaderRootProps = React.ComponentPropsWithRef<"header">;
+function HeaderRoot(props: HeaderRootProps) {
+  return <header {...props} />;
+}
+
+type HeaderNavProps = React.ComponentPropsWithRef<"ul">;
+function HeaderNav(props: HeaderNavProps) {
+  return (
+    <nav className="flex w-fit items-center justify-center">
+      <ul
+        {...props}
+        className={cn(
+          "flex list-none items-center justify-center gap-5",
+          props.className
+        )}
+      />
+    </nav>
+  );
+}
+
+type HeaderNavItemProps = React.ComponentPropsWithRef<"li"> & {
+  asChild?: boolean;
+};
+function HeaderNavItem({ asChild = false, ...props }: HeaderNavItemProps) {
+  const Component = asChild ? Slot : "li";
+
+  return <Component {...props} className={cn("", props.className)} />;
+}
+
+const Root = HeaderRoot;
+const Nav = HeaderNav;
+const NavItem = HeaderNavItem;
+
+export { Root, Nav, NavItem };
