133c733f028d49b8a5887aed856d8d02c6f90268
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-11-23 21:56:38 +0600

    chore: add fumadocs packages and modify next.config.ts.

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,14 @@
+import { createMDX } from "fumadocs-mdx/next";
 import type { NextConfig } from "next";
+
+const withMDX = createMDX({
+  configPath: "./src/registry/bricks/fuma-mdx/lib/source.config.ts",
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
