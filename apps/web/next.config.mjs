7f41123716643b88d27ddc08d16abde7c752f71d
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-11-29 10:47:37 +0600

    fix: turn next config to mjs..

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,11 +1,11 @@
 import { createMDX } from "fumadocs-mdx/next";
-import type { NextConfig } from "next";
 
 const withMDX = createMDX({
   configPath: "./src/registry/bricks/fuma-mdx/lib/source.config.ts",
 });
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
