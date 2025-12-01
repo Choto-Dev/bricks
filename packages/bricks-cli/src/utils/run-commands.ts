3592f78a9387e6ab7227980d13fb77c9a4f09b96
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-12-01 13:47:50 +0600

    feat(cli): add installNextjs function to install nextjs.

---
+++ packages/bricks-cli/src/utils/run-commands.ts
@@ -0,0 +1,127 @@
+import { type ChildProcess, spawn } from "node:child_process";
+import consola from "consola";
+import kill from "tree-kill";
+
+const activeProcesses = new Set<ChildProcess>();
+
+/**
+ * Run commands
+ * @param cmd Command
+ * @param args Command arguments
+ * @param rootDir Command spawn directory
+ * @returns Void promise
+ */
+function run(
+  cmd: string,
+  args: string[] = [],
+  rootDir: string = process.cwd(),
+) {
+  return new Promise<void>((resolve, reject) => {
+    const proc = spawn(cmd, args, {
+      cwd: rootDir,
+      shell: true,
+      stdio: "inherit",
+    });
+
+    activeProcesses.add(proc);
+
+    proc.on("close", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error("$cmdfailed with code $code"));
+      }
+    });
+
+    proc.on("error", (err) => {
+      activeProcesses.delete(proc);
+      reject(err);
+    });
+  });
+}
+
+/**
+ * Get the package manager name that is used to run the project.
+ * @returns npm package download command.
+ */
+function pkgManagerExecCommand() {
+  const pkgManager = process.env.npm_config_user_agent || "";
+
+  if (pkgManager.startsWith("pnpm") || pkgManager.startsWith("pnpx")) {
+    return "pnpm dlx";
+  }
+
+  return "npx";
+}
+
+const killAll = (signal: NodeJS.Signals = "SIGTERM") => {
+  for (const proc of activeProcesses) {
+    if (proc.pid) {
+      kill(proc.pid, signal);
+    }
+  }
+  activeProcesses.clear();
+  process.exit();
+};
+
+process.on("SIGINT", killAll);
+process.on("SIGTERM", killAll);
+process.on("exit", killAll);
+
+/**
+ * Install component from registry.
+ * @param componentUrl Json registry URL of the component.
+ * @param rootDir Root directory.
+ */
+async function installComponent(componentUrl: string) {
+  try {
+    await run(pkgManagerExecCommand(), ["shadcn@latest", "add", componentUrl]);
+  } catch (error) {
+    consola.error("❌", error);
+  }
+}
+
+/**
+ * Install nextjs with `create-next-app@latest` command.
+ * @param projectDirName - Project name and dir.
+ * @param options - Additional options.
+ * @param options.default - Use default setup for `create-next-app@latest` command.
+ */
+async function installNextjs(
+  projectDirName: string = "my-next-app",
+  options?: {
+    default?: boolean;
+  },
+) {
+  try {
+    if (options?.default) {
+      consola.info(
+        ` Running: ${pkgManagerExecCommand()} create-next-app@latest ${projectDirName} --typescript --tailwind --react-compiler --biome --app --src-dir --import-alias=@/* --turbopack`,
+      );
+      await run(pkgManagerExecCommand(), [
+        "create-next-app@latest",
+        projectDirName,
+        "--typescript",
+        "--tailwind",
+        "--react-compiler",
+        "--biome",
+        "--app",
+        "--src-dir",
+        "--import-alias=@/*",
+        "--turbopack",
+      ]);
+    } else {
+      consola.info(
+        ` Running: ${pkgManagerExecCommand()} create-next-app@latest ${projectDirName}`,
+      );
+      await run(pkgManagerExecCommand(), [
+        "create-next-app@latest",
+        projectDirName,
+      ]);
+    }
+  } catch (error) {
+    consola.error("❌", error);
+  }
+}
+
+export { installComponent, installNextjs };
